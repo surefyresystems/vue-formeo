1647a1390ba59cd822e05627c0fa61dd184af7ee
Dustin Torres <dustingtorres@gmail.com>
2018-10-06 14:42:47 -0700

    set SFC as main import

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <vue-formeo>
+    </vue-formeo>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import VueFormeo from './components/vue-formeo.vue'
+import VueFormeo from './components/vue-formeo.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    VueFormeo
   }
 }
 </script>
